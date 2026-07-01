003fa135463a06083d7b3929ec514fa0a3a6d3bf
ubdev2026 <accounts@unlibets.com>
2026-07-01 09:40:24 +0800

    env

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 
 // Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZH8e6GfRy9HM1bolhHcd8z2YEsFO1vrk",
-  authDomain: "ubapp-440f2.firebaseapp.com",
-  projectId: "ubapp-440f2",
-  storageBucket: "ubapp-440f2.firebasestorage.app",
-  messagingSenderId: "63410120475",
-  appId: "1:63410120475:web:84508dbbc899d1445e95be",
-  measurementId: "G-JBDQ3GHNMK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
